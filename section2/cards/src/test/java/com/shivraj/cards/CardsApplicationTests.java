d0c1e86a9bac7c82d803ff2f4157f2c8e41542fd
shivrajgodle <83442544+shivrajgodle@users.noreply.github.com>
2025-02-17 22:49:31 +0530

    Added Cards Service Rest Api's

---
+++ section2/cards/src/test/java/com/shivraj/cards/CardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivraj.cards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
